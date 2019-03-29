d417991f89de099cbc362c83d2ee3ed9311a40f6
zhoukaiheng <KF.zhoukaiheng@h3c.com>
2019-03-29 16:17:41 +0800

    build

---
+++ build/precache-manifest.665f081cf695cd56e31ca88b0cdb6c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2355b3f4009cb7a0d739",
+    "url": "/static/js/main.955b43b2.chunk.js"
+  },
+  {
+    "revision": "d00a86100a7d9b7d4ce9",
+    "url": "/static/js/2.3a147435.chunk.js"
+  },
+  {
+    "revision": "2355b3f4009cb7a0d739",
+    "url": "/static/css/main.b0191e45.chunk.css"
+  },
+  {
+    "revision": "d00a86100a7d9b7d4ce9",
+    "url": "/static/css/2.f6e4004d.chunk.css"
+  },
+  {
+    "revision": "2bf33acf23bc26cbf5f33d3763897094",
+    "url": "/index.html"
+  }
+];
